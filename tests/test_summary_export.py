6793184b711fa33e983401f3002ce5632db20077
Max <lowsky.coding@gmail.com>
2019-09-01 23:12:05 +0200

    Add test data

--- tests/test_summary_export.py
+++ tests/test_summary_export.py
@@ -11,24 +11,24 @@ except:
 class TestSummaryExport(unittest.TestCase):
 	def setUp(self):
 		try:
-			self.summary = summary_export.Summary('./data/169812TN.txt')
+			self.summary = summary_export.Summary('./data/test_data.txt')
 		except:
-			self.summary = summary_export.Summary('../data/169812TN.txt')
+			self.summary = summary_export.Summary('../data/test_data.txt')
 
 	def test_get_order_number(self):
-		self.assertEqual('4026252', self.summary.order_number[0])
+		self.assertEqual('1234567', self.summary.order_number[0])
 
 	def test_get_parent_lpn(self):
-		self.assertEqual('BUD322890', self.summary.parent_lpn[0])
+		self.assertEqual('DUMMYLPN1', self.summary.parent_lpn[0])
 
 	def test_get_product_id(self):
-		self.assertEqual('6684MC1543', self.summary.product_id[0])
+		self.assertEqual('DUMMYPID12', self.summary.product_id[0])
 
 	def test_get_quantity(self):
 		self.assertEqual('1', self.summary.quantity[0])
 
 	def test_get_serial_number(self):
-		self.assertEqual('13-54889346', self.summary.serial_number[0])
+		self.assertEqual('11-11111111', self.summary.serial_number[0])
 
 if __name__ == '__main__':
     unittest.main()
